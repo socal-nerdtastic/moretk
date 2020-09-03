59b4606cc1f1f591139d9cf44c39c36adf16397b
socalNerdtastic <socal.nerdtastic@gmail.com>
2020-09-02 23:23:29 -0700

    restructure to make arrow keys work

--- tkautocomplete.py
+++ tkautocomplete.py
@@ -1,46 +1,60 @@
 #!/usr/bin/env python3
 
+# TODO: SelectLabel add flag is_highlighted to avoid duplicate lowlight calls
+# TODO: figure out popup stay-on-top
+# TODO: popup needs to move with window, or disappear
+# TODO: popup width should match entry width
+
 import tkinter as tk
+from collections import deque
 
 class SelectLabel(tk.Frame):
     SELECTED_COLOR = 'light blue'
     HOVER_COLOR = 'teal'
     NORMAL_COLOR = 'white'
-    """turn color when hovered, allow for selection"""
+    """this widget is a single row item in the result list
+    turn color when hovered, allow for selection"""
     def __init__(self, master, **kwargs):
         self.text = kwargs.pop('text','')
         self.command = kwargs.pop('command', None)
         super().__init__(master, relief=tk.SUNKEN, bd=1, **kwargs)
         self.prefix = tk.Label(self, bd=0, padx=0, bg=self.NORMAL_COLOR)
         self.prefix.pack(side=tk.LEFT)
-        self.selected = tk.Label(self, bd=0, padx=0, bg=self.SELECTED_COLOR)
-        self.selected.pack(side=tk.LEFT)
+        self.select_core = tk.Label(self, bd=0, padx=0, bg=self.SELECTED_COLOR)
+        self.select_core.pack(side=tk.LEFT)
         self.rest = tk.Label(self, text=self.text, bd=0, padx=0, anchor=tk.W, bg=self.NORMAL_COLOR)
         self.rest.pack(fill=tk.X, expand=True)
-        self.rest.bind('<Button>', self._mouse_click)
-        self.selected.bind('<Button>', self._mouse_click)
-        self.prefix.bind('<Button>', self._mouse_click)
-        self.bind('<Enter>', self._mouse_enter)
-        self.bind('<Leave>', self._mouse_leave)
+        self.rest.bind('<Button>', self.choose)
+        self.select_core.bind('<Button>', self.choose)
+        self.prefix.bind('<Button>', self.choose)
+        self.bind('<Enter>', self.highlight)
+        self.bind('<Leave>', self.lowlight)
 
-    def _mouse_click(self, event=None):
+        self.next = None
+        self.previous = None
+
+    def choose(self, event=None):
         if self.command:
             self.command(self.text)
 
-    def _mouse_enter(self, event=None):
+    def highlight(self, event=None):
         if self.master.selected is not None:
-            self.master.move_none()
+            self.master.selected.lowlight()
+        self.master.selected = self
+
         self.prefix.config(bg=self.HOVER_COLOR)
-        self.selected.config(bg=self.HOVER_COLOR)
+        self.select_core.config(bg=self.HOVER_COLOR)
         self.rest.config(bg=self.HOVER_COLOR)
 
-    def _mouse_leave(self, event=None):
+    def lowlight(self, event=None):
+        # will be called twice
+        # by the mouse leave AND other.mouse enter ... i'm ok with this (for now)
         self.prefix.config(bg=self.NORMAL_COLOR)
-        if self.selected['text']:
-            self.selected.config(bg=self.SELECTED_COLOR)
+        if self.select_core['text']:
+            self.select_core.config(bg=self.SELECTED_COLOR)
         else:
             # this case should never happen, zero matches should delete the label.
-            self.selected.config(bg=self.NORMAL_COLOR)
+            self.select_core.config(bg=self.NORMAL_COLOR)
         self.rest.config(bg=self.NORMAL_COLOR)
 
     def select(self, start=None, end=None):
@@ -57,45 +71,44 @@ class SelectLabel(tk.Frame):
         if end is None:
             start, end = 0, start
         self.prefix.config(text=self.text[:start])
-        self.selected.config(text=self.text[start:end])
+        self.select_core.config(text=self.text[start:end])
         self.rest.config(text=self.text[end:])
 
 def startswith(whole_phrase, search_phrase):
     if whole_phrase.startswith(search_phrase):
         return 0, len(search_phrase)
 
+def startswith_ignorecase(whole_phrase, search_phrase):
+    if whole_phrase.casefold().startswith(search_phrase.casefold()):
+        return 0, len(search_phrase)
+
 class OptionBox(tk.Frame):
+    """the popup widget"""
     def __init__(self, master, options=[], command=None, **kwargs):
         super().__init__(master, **kwargs)
 
         self.sublabels = {}
+        self.first = None
         self.command = command
         self.remake(options)
         self.selected = None
 
     def move_down(self):
-        if self.selected is None:
-            self.selected = 0
-        else:
-            self.selected += 1
-        self.highlight()
+        if self.selected is None and self.first is not None:
+            self.first.highlight()
+        elif self.selected is not None:
+            self.selected.next.highlight()
+
     def move_up(self):
-        if self.selected is None:
-            self.selected = -1
-        else:
-            self.selected -= 1
-        self.highlight()
-    def move_none(self):
-        self.selected = None
-        self.highlight()
-    def highlight(self):
+        if self.selected is None and self.first is not None:
+            self.first.previous.highlight()
+        elif self.selected is not None:
+            self.selected.previous.highlight()
+
+    def lowlight(self):
         if self.selected is not None:
-            self.selected %= len(self.sublabels)
-        for i, child in enumerate(self.sublabels.values()):
-            if self.selected == i:
-                child._mouse_enter()
-            else:
-                child._mouse_leave()
+            self.selected.lowlight()
+        self.selected = None
 
     def remake(self, options=[]):
         sublabels = {}
@@ -108,12 +121,27 @@ class OptionBox(tk.Frame):
             lbl.pack(expand=True, fill=tk.X)
             lbl.select(match)
             sublabels[text] = lbl
-        for text, child in list(self.sublabels.items()):
+
+        # set up LL
+        labels = list(sublabels.values())
+        if labels:
+            self.first = labels[0]
+            for a, b in zip(labels, labels[1:] + [labels[0]]):
+                a.next = b
+            for a, b in zip(labels, [labels[-1]] + labels[:-1]):
+                a.previous = b
+        else:
+            self.first = None
+
+        # delete all remaining labels
+        for child in self.sublabels.values():
             child.destroy()
+
         self.sublabels = sublabels
 
-class SearchEntry(tk.Entry):
-    def __init__(self, master, options=None, hitlimit=50, func=startswith, **kwargs):
+
+class AutoComplete(tk.Entry):
+    def __init__(self, master, options=None, hitlimit=50, func=startswith_ignorecase, **kwargs):
         self.var = kwargs.pop('textvariable', tk.StringVar(master))
         super().__init__(master, textvariable=self.var, **kwargs)
         self.var.trace('w', self._on_change)
@@ -125,6 +153,11 @@ class SearchEntry(tk.Entry):
 
         self.bind('<Down>', self.move_down)
         self.bind('<Up>', self.move_up)
+        self.bind('<Return>', self.on_return)
+
+    def on_return(self, event=None):
+        if self.optionbox and self.optionbox.selected:
+            self.optionbox.selected.choose()
 
     def move_down(self, event=None):
         if self.optionbox:
@@ -143,7 +176,7 @@ class SearchEntry(tk.Entry):
         P = self.var.get()
 
         if self.optionbox:
-            self.optionbox.move_none()
+            self.optionbox.lowlight()
 
         if not P:
             return self._close_popup()
@@ -192,19 +225,14 @@ class SearchEntry(tk.Entry):
         root_y = self.winfo_rooty() + y
         self.popup.wm_geometry("+%d+%d" % (root_x, root_y))
 
-data = '''\
-Documentation is like sex.
-When it's good,
-it's very good.
-When it's bad,
-it's better than nothing.
-When it lies to you,
-it may be a while before you realize something's wrong'''.lower().splitlines()
-
 def main():
+    # test / demo
+    with open('fruit.txt') as f:
+        data = f.read().splitlines()
     root = tk.Tk()
-    tk.Label(root, text='Type "when" or "it"').pack()
-    box = SearchEntry(root, options=data)
+    tk.Label(root, text='Type a fruit').pack()
+    box = AutoComplete(root, options=data)
+    box.focus()
     box.pack()
     root.mainloop()
 
